3e7b2ef88caeef32e39facb7948f0c5c572ad0a0
maurinElectroTextile <maurin@datapaulette.org>
2018-05-09 15:25:59 +0200

    Minor change

--- Arduino/Calibration/endLignes/endLignes.ino
+++ Arduino/Calibration/endLignes/endLignes.ino
@@ -5,13 +5,13 @@ int endLineLeftAPin = 1;
 int endLineRightAPin = 0;
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(38400);
 }
 
 void loop(){
   int valueEndLineLeft = analogRead(endLineLeftAPin);
   int valueEndLineRight = analogRead(endLineRightAPin);
   Serial.print(valueEndLineLeft);
-  Serial.print("-");
+  Serial.print("\t");
   Serial.println(valueEndLineRight);
 }
